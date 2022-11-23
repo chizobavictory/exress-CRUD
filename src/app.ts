478b98b1d5d4773f01aba9fd78dbc3f32f9d4034
Chizoba Victory <chizoba.victory1@gmail.com>
2022-11-23 22:03:38 +0100

    added module exports

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
-import express, { Express, NextFunction, Request, Response } from 'express';
-import createError, { HttpError } from 'http-errors';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import createError from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-import dotenv from "dotenv";
-
-dotenv.config();
-
-
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
 
 const app = express();
 
@@ -22,18 +17,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req: Request, res: Response, next: NextFunction) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
+app.use(function(err:createError.HttpError, req:Request, res:Response, next:NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,4 +38,4 @@ app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction
   res.render('error');
 });
 
-export default app;
+export default app
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
-import express, { Express, Request, Response, NextFunction } from 'express';
-
+import express, {Request, Response} from 'express';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req: Request, res: Response, next:  NextFunction) {
+router.get('/', function(req:Request, res:Response, next) {
   res.render('index', { title: 'Express' });
 });
 
-export default router;
+export default router
